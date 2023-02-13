12b3d7a71fe74fa58c48a0633a52cef6911b2c0f
luwasd <lmanuel14@gmail.com>
2023-02-13 18:39:32 -0300

    First Commit

---
+++ src/components/Componente.jsx
@@ -0,0 +1,15 @@
+const Componente = () => {
+    return (
+        <>
+            <h1>Hello Dojo!</h1>
+            <h2>Things I need to do:</h2>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </>
+    );
+}
+export default Componente;
---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Componente from './components/Componente';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Componente/>
+  </React.StrictMode>
+);
+
